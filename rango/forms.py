7fda60e9803fc33ad6c1f31d25f68a01c0d4b171
Robert Pringle <2304777P@student.gla.ac.uk>
2019-02-06 18:02:13 +0000

    Ch10.1 commit

--- rango/forms.py
+++ rango/forms.py
@@ -18,6 +18,10 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	
+	  class Meta:
+        model = Page
+        exclude = ('category',)
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -29,9 +33,7 @@ class PageForm(forms.ModelForm):
 
             return cleaned_data
 
-    class Meta:
-        model = Page
-        exclude = ('category',)
+  
 		
 class UserForm(forms.ModelForm):
 	assword = forms.CharField(widget=forms.PasswordInput())
